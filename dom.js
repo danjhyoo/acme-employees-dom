d70c4812441999eb48296300ebf9ba7e62aa7e28
Dan Yoo <danjhyoo@gmail.com>
2020-01-23 21:31:41 -0500

    final

--- dom.js
+++ dom.js
@@ -9,10 +9,63 @@ const employees = [
     { id: 99, name: 'lucy' }
 ];
 
-const container = document.querySelector('#container');
+const list = document.querySelector('#employees');
 
-employees.forEach((employee) => {
-    const div = document.createElement('div')
-    div.innerHTML = employee.name;
-    container.appendChild(div);
+
+// list.addEventListener('click', (ev) => {
+//     // console.log(ev.target)
+//     if (ev.target.tagName === 'LI' && ev.target.style.backgroundColor === 'lightskyblue') {
+//         ev.target.style.backgroundColor = 'salmon'
+//     } else {
+//         ev.target.style.backgroundColor = 'lightskyblue'
+//     }
+//     // if(ev.target.style.backgroundColor = 'salmon') {
+//     //     ev.target.style.backgroundColor =
+//     // }
+// })
+let count = 0
+
+list.addEventListener('click', (ev) => {
+    const target = ev.target;
+    if (target.tagName === 'LI') {
+        const idx = [...list.children].indexOf(target);
+        const numOfFavorites = employees.filter(employee => {
+            return employee.favorite;
+        }).length
+
+        if (!target.classList.contains('favorite') && numOfFavorites === 3) {
+            alert('only 3 favorites!');
+
+        } else {
+            employees[idx].favorite = !employees[idx].favorite;
+        }
+
+        // if (employees[idx].favorite) {
+        //     count--
+        //     employees[idx].favorite = !employees[idx].favorite;
+        // } else if (count < 3) {
+        //     count++
+        //     employees[idx].favorite = !employees[idx].favorite;
+        // } else {
+        //     alert('only 3 favorites');
+        // }
+
+    }
+    render();
 })
+
+
+
+
+
+const render = () => {
+    const html = employees.map(employee => {
+        return `<li data-id='${employee.id}' ${employee.favorite ? "class='favorite'" : ''}>${employee.name}</li>`;
+    }).join('');
+
+    list.innerHTML = html;
+}
+
+
+
+render();
